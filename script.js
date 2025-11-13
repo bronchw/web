f1374456d94935a111b3c646aced7d3ab4b415c3
bronchw <bronislav@chwastek.cz>
2025-11-13 23:41:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const images = ["img1.png", "img2.png", "img3.png", "img4.png"];
+let current = 0;
+
+function changeImage() {
+  current = (current + 1) % images.length;
+  document.getElementById("slideshow").src = images[current];
+}
+
+// změna obrázku každých 250 ms (můžeš změnit)
+setInterval(changeImage, 250);
