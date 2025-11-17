34e8e408b5b7ffefb18ce87c6ae7645139fb28e6
bronchw <bronislav@chwastek.cz>
2025-11-17 21:42:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const images = [
   "img1.png", "img2.png", "img3.png", "img4.png",
-  "img5.png", "img6.png", "img7.png", "img8.png", "img9.png"
+  "img5.png", "img6.png", "img7.png"
 ];
 
 let current = 0;
