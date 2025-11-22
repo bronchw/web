9e0b3263770b4b471baddcaeebb5a0c46ad180d7
chwastek <bronislav@chwastek.cz>
2025-11-22 15:37:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+
 const images = [
   "img1.png", "img2.png", "img3.png", "img4.png",
   "img5.png", "img6.png", "img7.png"
 ];
 
+
+const preloaded = [];
+for (let i = 0; i < images.length; i++) {
+  preloaded[i] = new Image();
+  preloaded[i].src = images[i];
+}
+
 let current = 0;
 
 function changeImage() {
   current = (current + 1) % images.length;
-  document.getElementById("slideshow").src = images[current];
+
+
+  document.getElementById("mainAnimation").src = preloaded[current].src;
 }
+
 
 setInterval(changeImage, 220);
