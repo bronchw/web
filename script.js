3944fb0554cc9064aa26559aad3e010126dae8f7
chwastek <bronislav@chwastek.cz>
2025-11-24 00:27:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
 const images = [
   "img1.png", "img2.png", "img3.png", "img4.png",
   "img5.png", "img6.png", "img7.png"
 ];
-
 
 const preloaded = [];
 for (let i = 0; i < images.length; i++) {
@@ -15,22 +13,11 @@ let current = 0;
 
 function changeImage() {
   current = (current + 1) % images.length;
-
-
-  document.getElementById("mainAnimation").src = preloaded[current].src;
+  document.getElementById("mainAnimation").style.backgroundImage =
+    `url('${preloaded[current].src}')`;
 }
-
 
 setInterval(changeImage, 220);
 
-const img = document.getElementById("mainAnimation");
-
-img.addEventListener("contextmenu", e => e.preventDefault());
-
-img.addEventListener("touchstart", e => {
-    if (e.touches.length > 0) {
-        e.preventDefault();
-    }
-}, { passive: false });
-
-document.addEventListener("gesturestart", e => e.preventDefault());
+document.getElementById("mainAnimation").style.backgroundImage =
+  `url('${images[0]}')`;
